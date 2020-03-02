928b6b9d9219324f09c514489d79f4af41c768e6
Tom Jardine-McNamara <tom@itsravenous.com>
2020-03-02 00:24:17 +0000

    add custom name feature
    Disappointingly, jsdom doesn't seem to support even client-side
    navigation, so the tests for navigating to the search feature are based
    on providing an initial route rather than clicking the search button.
    With more time I'd add some tests to assert that the button is at least
    requesting the correct URL change.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Router, navigate } from '@reach/router';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,24 +13,73 @@ const apiBaseUrl = 'http://api.icndb.com';
 function App() {
   const [joke, setJoke] = React.useState(initialMessage);
   const [error, setError] = React.useState();
-  const fetchRandomJoke = async () => {
+
+  const fetchRandomJoke = async ({ firstName, lastName }) => {
+    let url = `${apiBaseUrl}/jokes/random?exclude=[explicit]&escape=javascript`;
+    if (firstName) url += `&firstName=${firstName}`;
+    if (lastName) url += `&lastName=${lastName}`;
     try {
-      const { data } = await axios.get(
-        `${apiBaseUrl}/jokes/random?exclude=[explicit]&escape=javascript`
-      );
+      const { data } = await axios.get(url);
       setJoke(data.value.joke);
     } catch {
       setError(errorMessage);
       setJoke('');
     }
   };
+
+  const gotoSearch = () => {
+    navigate('/search');
+  };
+
   return (
     <div className="App">
       <button onClick={fetchRandomJoke}>Random joke</button>
+      <button onClick={gotoSearch}>Search jokes</button>
       {joke}
       {error}
+      <Router>
+        <SearchForm
+          path="/search"
+          onSubmit={({ firstName, lastName }) => {
+            fetchRandomJoke({ firstName, lastName });
+          }}
+        />
+      </Router>
     </div>
   );
 }
+
+const SearchForm = ({ onSubmit }) => {
+  const [name, setName] = React.useState('');
+  const [isValid, setIsValid] = React.useState(false);
+
+  const handleNameChange = name => {
+    setName(name);
+    setIsValid(name.length > 0 && name.split(' ').length < 3);
+  };
+  const handleSubmit = name => {
+    const [firstName, lastName] = name.split(' ');
+    onSubmit({ firstName, lastName });
+  };
+
+  return (
+    <>
+      <label htmlFor="name">Enter an inferior name</label>
+      <input
+        id="name"
+        type="text"
+        onChange={e => handleNameChange(e.target.value)}
+        value={name}
+      />
+      <button
+        disabled={!isValid}
+        onClick={() => handleSubmit(name)}
+        type="submit"
+      >
+        Submit
+      </button>
+    </>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,54 +1,151 @@
 import React from 'react';
 import nock from 'nock';
+import userEvent from '@testing-library/user-event';
 import { render, fireEvent } from '@testing-library/react';
+import {
+  createHistory,
+  createMemorySource,
+  LocationProvider
+} from '@reach/router';
 import App from './App';
 
-const mockJoke = {
+const createMockJoke = jokeText => ({
   type: 'success',
   value: {
     id: 144,
-    joke: 'Mock joke content here 😂',
+    joke: jokeText,
     categories: []
   }
-};
-const queryMatcher = query =>
-  query.exclude === '[explicit]' && query.escape === 'javascript';
-
-it('displays an appropriate initial message', () => {
-  const { getByText } = render(<App />);
-  getByText(
-    "If Chuck Norris was using this application, he'd have clicked the joke button by now"
-  );
 });
 
-it('fetches a random joke when the button is clicked, excluding explicit jokes', async () => {
-  const scope = nock('http://api.icndb.com')
-    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-    .get('/jokes/random')
-    .query(queryMatcher)
-    .reply(200, mockJoke);
-  const { getByText, findByText } = render(<App />);
+describe('Random joke feature', () => {
+  const queryMatcher = query =>
+    query.exclude === '[explicit]' &&
+    query.escape === 'javascript' &&
+    typeof query.firstName === 'undefined' &&
+    typeof query.lastName === 'undefined';
 
-  const button = getByText(/random joke/i);
-  fireEvent.click(button);
+  it('displays an appropriate initial message', () => {
+    const { getByText } = render(<App />);
+    getByText(
+      "If Chuck Norris was using this application, he'd have clicked the joke button by now"
+    );
+  });
 
-  await findByText('Mock joke content here 😂');
-  expect(scope.isDone()).toBe(true);
+  it('fetches a random joke when the button is clicked, excluding explicit jokes', async () => {
+    const scope = nock('http://api.icndb.com')
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/jokes/random')
+      .query(queryMatcher)
+      .reply(200, createMockJoke('Mock joke content here 😂'));
+    const { getByText, findByText } = render(<App />);
+
+    const randomJokeButton = getByText(/random joke/i);
+    fireEvent.click(randomJokeButton);
+
+    await findByText('Mock joke content here 😂');
+    expect(scope.isDone()).toBe(true);
+  });
+
+  it('displays an appropriate error message when the random joke service call fails', async () => {
+    const scope = nock('http://api.icndb.com')
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/jokes/random')
+      .query(queryMatcher)
+      .reply(500);
+    const { getByText, findByText } = render(<App />);
+
+    const button = getByText(/random joke/i);
+    fireEvent.click(button);
+
+    await findByText(
+      'Chuck Norris has declined your ridiculous request for a joke 🤠'
+    );
+    expect(scope.isDone()).toBe(true);
+  });
 });
 
-it('displays an appropriate error message when the random joke service call fails', async () => {
-  const scope = nock('http://api.icndb.com')
-    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-    .get('/jokes/random')
-    .query(queryMatcher)
-    .reply(500);
-  const { getByText, findByText } = render(<App />);
+const renderWithRouter = (
+  ui,
+  { route = '/', history = createHistory(createMemorySource(route)) } = {}
+) => render(<LocationProvider history={history}>{ui}</LocationProvider>);
 
-  const button = getByText(/random joke/i);
-  fireEvent.click(button);
+describe('Joke search feature', () => {
+  it('requests a joke with a custom single-word name', async () => {
+    const jokeText =
+      "Why won't Chuck Norris go to dinner with Cher? Because she doesn't like to Cher her food! 🤣";
+    const scope = nock('http://api.icndb.com')
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/jokes/random')
+      .query(
+        query =>
+          query.exclude === '[explicit]' &&
+          query.escape === 'javascript' &&
+          query.firstName === 'Cher'
+      )
+      .reply(200, createMockJoke(jokeText));
+    const { getByText, findByLabelText, findByText } = renderWithRouter(
+      <App />,
+      {
+        route: '/search'
+      }
+    );
 
-  await findByText(
-    'Chuck Norris has declined your ridiculous request for a joke 🤠'
-  );
-  expect(scope.isDone()).toBe(true);
+    const searchInput = await findByLabelText(/enter an inferior name/i);
+    await userEvent.type(searchInput, 'Cher');
+
+    const searchSubmitButton = getByText(/submit/i);
+    fireEvent.click(searchSubmitButton);
+    await findByText(jokeText);
+  });
+
+  it('requests a joke with a custom first and last name', async () => {
+    const jokeText =
+      'What did Arnie say when asked about Windows 10? I still love Vista baby.';
+    const scope = nock('http://api.icndb.com')
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/jokes/random')
+      .query(
+        query =>
+          query.exclude === '[explicit]' &&
+          query.escape === 'javascript' &&
+          query.firstName === 'Arnold' &&
+          query.lastName === 'Schwarznegger'
+      )
+      .reply(200, createMockJoke(jokeText));
+    const { getByText, findByLabelText, findByText } = renderWithRouter(
+      <App />,
+      {
+        route: '/search'
+      }
+    );
+
+    const searchInput = await findByLabelText(/enter an inferior name/i);
+    await userEvent.type(searchInput, 'Arnold Schwarznegger');
+
+    const searchSubmitButton = getByText(/submit/i);
+    fireEvent.click(searchSubmitButton);
+    await findByText(jokeText);
+  });
+
+  it('prevents submitting the form if no name is entered', async () => {
+    const { getByText, findByLabelText } = renderWithRouter(<App />, {
+      route: '/search'
+    });
+
+    const searchSubmitButton = getByText(/submit/i);
+    expect(searchSubmitButton).toBeDisabled();
+  });
+
+  it('prevents submitting the form if more than two words are entered', async () => {
+    const { getByText, findByLabelText } = renderWithRouter(<App />, {
+      route: '/search'
+    });
+
+    const searchInput = await findByLabelText(/enter an inferior name/i);
+    await userEvent.type(searchInput, 'Mary Elizabeth Mastrantonio');
+
+    const searchSubmitButton = getByText(/submit/i);
+    expect(searchSubmitButton).toBeDisabled();
+  });
 });
